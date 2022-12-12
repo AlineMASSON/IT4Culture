45d5984571c667eb74fdee7d0342f2667511b2a7
AlineMASSON <lacmas03@gmail.com>
2022-12-12 16:35:47 +0100

    connexion bdd

---
+++ public/index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/reset.css">
+    <link rel="stylesheet" href="assets/styles.css">
+    <title>IT4Culture</title>
+</head>
+<body>
+    <?php 
+        $configData = parse_ini_file(__DIR__.'/../config.ini');
+
+        try {
+            $dbh = new PDO(
+                "mysql:host={$configData['DB_HOST']};dbname={$configData['DB_NAME']};charset=utf8",
+                $configData['DB_USERNAME'],
+                $configData['DB_PASSWORD'],
+                array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING)
+            );
+            echo 'Connexion réussie';
+        }
+        catch(\Exception $exception) {
+            echo 'Erreur de connexion...<br>';
+            echo $exception->getMessage().'<br>';
+            echo '<pre>';
+            echo $exception->getTraceAsString();
+            echo '</pre>';
+            exit;
+        }
+    ?>
+    <div class="research">
+        <h1 class="research__title">Productions</h1>
+        <select class="research__values" name="productions" id="productions">
+            <option value="1">Tosca</option>
+            <option value="3">Casse Noisette</option>
+            <option value="4">Don Giovanni</option>
+        </select>
+    </div>
+    <div class="informations">
+        <div class="informations__header">
+            <h2 class="header__title">Tosca</h2>
+            <h3 class="header__name">Giuseppe Verdi</h3>
+            <p class="header__dates">du 11 au 21 janvier 2022</p>
+        </div>
+        <div class="informations__representations">
+            <h2 class="representations__title">Représentations :</h2>
+            <p class="representations__date">Mardi 11 janvier à 19h30</p>
+            <p class="representations__date">Jeudi 13 janvier à 19h30</p>
+            <p class="representations__date">Samedi 11 janvier à 19h30</p>
+            <p class="representations__date">Jeudi 20 janvier à 19h30</p>
+            <p class="representations__date">Vendredi 21 janvier à 19h30</p>
+        </div>
+        <div class="informations__cast">
+            <h2 class="cast__title">Distribution : <img src="assets/img/person-plus-fill.svg" alt="add performer"> </h2>
+            <div class="cast__name">
+                <p class="name__character">Tosca</p>
+                <p class="name__performer">Marie Lemieux</p>
+            </div>
+            <div class="cast__name">
+                <p class="name__character">Mario Cavaradossi</p>
+                <p class="name__performer">Anja Harteros</p>
+            </div>
+            <div class="cast__name">
+                <p class="name__character">Scarpia</p>
+                <p class="name__performer">Marcelo Spuente</p>
+            </div>
+            <div class="cast__name">
+                <p class="name__character">Cesare Angelotti</p>
+                <p class="name__performer">Luca Salsi</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
